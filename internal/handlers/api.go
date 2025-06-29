d006716116f9be944439b24ed4faf694f10659aa
NodeSeeker <145534855+nodeseeker@users.noreply.github.com>
2025-06-29 12:45:40 +0800

    fix webp

--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -153,16 +153,32 @@ func HandleAPIUpload(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// 上传到Telegram
-	msg := tgbotapi.NewDocument(global.AppConfig.Telegram.ChatID, tgbotapi.FilePath(tempFile.Name()))
-	message, err := global.Bot.Send(msg)
-	if err != nil {
-		sendJSONError(w, "上传到存储服务失败", http.StatusInternalServerError)
-		return
-	}
+	// 根据文件类型选择发送方式
+	var message tgbotapi.Message
+	var fileID string
 
-	// 获取文件URL
-	fileID := message.Document.FileID
+	// 对于图片文件，使用NewPhoto发送以确保在Telegram中正确显示
+	if contentType == "image/jpeg" || contentType == "image/jpg" || contentType == "image/png" || contentType == "image/webp" {
+		photoMsg := tgbotapi.NewPhoto(global.AppConfig.Telegram.ChatID, tgbotapi.FilePath(tempFile.Name()))
+		message, err = global.Bot.Send(photoMsg)
+		if err != nil {
+			sendJSONError(w, "上传到存储服务失败", http.StatusInternalServerError)
+			return
+		}
+		// 获取最大尺寸的照片文件ID
+		if len(message.Photo) > 0 {
+			fileID = message.Photo[len(message.Photo)-1].FileID
+		}
+	} else {
+		// 对于GIF等其他格式，仍使用Document方式
+		docMsg := tgbotapi.NewDocument(global.AppConfig.Telegram.ChatID, tgbotapi.FilePath(tempFile.Name()))
+		message, err = global.Bot.Send(docMsg)
+		if err != nil {
+			sendJSONError(w, "上传到存储服务失败", http.StatusInternalServerError)
+			return
+		}
+		fileID = message.Document.FileID
+	}
 	telegramURL, err := global.Bot.GetFileDirectURL(fileID)
 	if err != nil {
 		sendJSONError(w, "获取文件URL失败", http.StatusInternalServerError)
